fe50172494af3ff68d734d1724e28e771bb58c63
Kat Leedy <katleedy@unc.edu>
2022-09-01 18:10:57 -0400

    Progress on Exercise 1

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,41 @@
+"""Another step towards a Wordle game."""
+
+__author__ = "730610012"
+
+from turtle import color
+
+
+secret: str = "ancient"
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+user_guess: str = input(f"What is your {len(secret)}-letter guess?")
+while len(user_guess) != len(secret):
+    user_guess = input("That was not 6 letters! Try again:")
+
+i: int = 0
+color_result: str = ""
+
+while i < len(secret):
+    letter_in_secret: bool = False
+    j: int = 0
+    while j < len(secret):
+        if user_guess[i] == secret[j]:
+            letter_in_secret = True
+        j += 1
+    
+    if user_guess[i] == secret[i]:
+        color_result += GREEN_BOX
+    elif letter_in_secret == True:
+        color_result += YELLOW_BOX
+    else:
+        color_result += WHITE_BOX
+    i += 1
+
+print(color_result)
+
+if color_result == GREEN_BOX*len(secret):
+    print("Woo! You got it!")
+else: 
+    print("Not quite. Play again soon!")
