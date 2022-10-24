39f20312e4337e346b8ee163ba676b6c617269d0
Kat Leedy <katleedy@unc.edu>
2022-10-24 19:45:54 -0400

    part 2/3 of changes as of 10/24/22

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,5 +1,13 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730610012"
 
+
+from io import TextIOWrapper
 # Define your functions below
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Reads each row from csv file into a list of str-str dictionaries"""
+    row_table: list[dict[str, str]] = []
+
+
+    return row_table
