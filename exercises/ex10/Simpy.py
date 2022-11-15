afcb63c1a3f3f761101b720a85fa45af69295aa9
Kat Leedy <katleedy@unc.edu>
2022-11-14 23:15:11 -0500

    lessons and exercises on magic methods

--- exercises/ex10/Simpy.py
+++ exercises/ex10/Simpy.py
@@ -4,10 +4,114 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730610012"
 
 
 class Simpy:
+    """A class of utility methods for working with sequences of numerical data."""
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]):
+        """Constructs a Simpy object and initiates its values attribute to a list of floats."""
+        self.values = values
+    
+    def __repr__(self) -> str:
+        """Returns a string representation of a Simpy object."""
+        return f"Simpy({self.values})"
+
+    def fill(self, value: float, quant: int) -> None:
+        """Fills the Simpy object with a set number of a certain float value."""
+        self.values.clear()
+        for _ in range(quant):
+            self.values.append(value)
+    
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        """Fills the values attribute with a range of float values from start (inclusive) to stop (not inclusive), by an optional step."""
+        assert step != 0.0
+        self.values.clear()
+        i: float = start
+        if step > 0.0:
+            while i < stop:
+                self.values.append(i)
+                i += step
+        else:
+            while i > stop:
+                self.values.append(i)
+                i += step
+    
+    def sum(self) -> float:
+        """Adds up all values in the values attribute."""
+        total: float = sum(self.values)
+        return total
+
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Adds the corresponding value of rhs to each item of the values attribute."""
+        total: Simpy = Simpy([])
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                total.values.append(self.values[i] + rhs.values[i])
+        else:
+            for value in self.values:
+                total.values.append(value + rhs)
+        return total
+
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Raises each item of the values attribute to the power of the corresponding value of rhs."""
+        total: Simpy = Simpy([])
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                total.values.append(self.values[i] ** rhs.values[i])
+        else:
+            for value in self.values:
+                total.values.append(value ** rhs)
+        return total
+
+    def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Returns a mask of bool indicating whether the corresponding value of rhs equals each item of the values attribute."""
+        mask: list[bool] = []
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                if self.values[i] == rhs.values[i]:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+        else:
+            for value in self.values:
+                if value == rhs:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+        return mask
+
+    def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Returns a mask of bool indicating whether each item of the values attribute is greater than the corresponding value of rhs."""
+        mask: list[bool] = []
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                if self.values[i] > rhs.values[i]:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+        else:
+            for value in self.values:
+                if value > rhs:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+        return mask
+
+    def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Returns an indicated value of values or a new Simpy object with items that correspond to 'True' in a mask."""
+        if isinstance(rhs, int):
+            return self.values[rhs]
+        else:
+            assert len(self.values) == len(rhs)
+            masked: Simpy = Simpy([])
+            for i in range(len(self.values)):
+                if rhs[i]:
+                    masked.values.append(self.values[i])
+            return masked
--- lessons/point.py
+++ lessons/point.py
@@ -16,12 +16,11 @@ class Point:
         self.x *= factor
         self.y *= factor
     
-    def scale(self, factor: float) -> Point:
-        """Immutable: multiplies components by factor."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+    # def scale(self, factor: float) -> Point:
+    #     """Immutable: multiplies components by factor."""
+    #     return Point(self.x * factor, self.y * factor)
+
+    # new magic method __mul__ does the function of scale()
 
     def __str__(self) -> str:
         """Produce a str representation of a Point for humans."""
@@ -31,10 +30,28 @@ class Point:
         """Produce a str representation of a Point for Python!"""
         return f"Point({self.x}, {self.y})"
         # repr is more for representing code as strings to import/export objects into different files
+    
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for a Point multiplied by a float."""
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        print("__add__ was called")
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    # Could use this to overload subscription notation, although it is not good design in this case.
+    def __getitem__(self, index: int) -> float:
+        """Overload the subscription notation."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError
 
 
 p0: Point = Point(1.0, 2.0)
-p1: Point = p0.scale(2.0)
+p1: Point = p0 * 2.0
 print(p0)
 # str() method looks for the __str__ magic method to represent an object as a str
 # print() calls str() behind the scenes
@@ -43,3 +60,8 @@ print(p1_as_a_str)
 
 p1_repr: str = repr(p1)
 print(p1_repr)
+
+p_total: Point = p0 + p1
+print(p_total)
+
+print(p_total[0])
